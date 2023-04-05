a4a2e2612d827ad15f19696baad0fdbf5ac210f9
amilpsp <amanda.m.ruiz@gmail.com>
2023-04-05 18:45:58 +0200

    Removed main methods to test other exercises

--- src/Failed_Problem14.java
+++ src/Failed_Problem14.java
@@ -1,31 +1,7 @@
 public class Failed_Problem14 {
 /*Write a function to find the longest common prefix string amongst an array of strings.
 If there is no common prefix, return an empty string "".*/
-    public static void main (String[] args){
-        String[] strAr1=new String[] {"Ani", "Sam", "Joel"};
-        String longestCP = longestCommonPrefix(strAr1);
-        System.out.print(longestCP);
-    }
-        public static String longestCommonPrefix(String[] strings) {
-        int biggestLength = getLongestStringsLenght(strings);
-        for (int i =0; i<strings.length;i++){
-            for (int j=0; j<biggestLength;j++){
-                if (strings[i].charAt(j)==strings[i+1].charAt(j)){
 
-
-                }
-            }
-        }
-        /*    char prevChar=strs[0].charAt(0);
-            int k=0;
-
-            for (int i=0; i < strs.length; i++){    //characters
-                for (int j=0; j< strs.length; j++){ //words
-                    allLetters+=strs[j].charAt(i);
-                }
-            }*/
-            return allLetters;
-        }
     public static int getLongestStringsLenght(String []strings) {
         //  String strings[] =  {"cat","chicken","horse","ooooooooo" };
         int biggestLength=0;
--- src/Problem1281.java
+++ src/Problem1281.java
@@ -1,19 +1,7 @@
 import java.util.Scanner;
 
 public class Problem1281 {
-    public static void main(String[]args){
 
-        Scanner numberScan = new Scanner(System.in);
-
-        int number = 0;
-        while (number <=0){
-            System.out.print("gimme a number plz: ");
-            number = numberScan.nextInt();
-        }
-
-        int difference = subtractProductAndSum(number);
-        System.out.print("The difference between the Product and the sum of it's digits is " + difference );
-    }
     public static int subtractProductAndSum(int n) {
         int nSum = n;
         int nProd = n;
