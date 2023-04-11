ef783537e2e67d88ad7ba8c2547fbac328569878
amilpsp <amanda.m.ruiz@gmail.com>
2023-04-11 22:14:08 +0200

    terrible memory usage and runtime, but accepted submit

--- src/Success_Problem1232.java
+++ src/Success_Problem1232.java
@@ -1,17 +1,15 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class Current_Problem1232 {
-    public static void main(String[]args){
+public class Success_Problem1232 {
+ /*   public static void main(String[]args){
         int[][] coordinates = {{2,1},{4,2},{6,3}};
         boolean straight = checkStraightLine(coordinates);
         System.out.println(straight);
-    }
+    }*/
     public static boolean checkStraightLine(int[][] coordinates) {
         Arrays.sort(coordinates, Comparator.comparingInt(x -> x[0]));
-        for (int[] point:coordinates){
-            System.out.println(point[0]+","+point[1]);
-        }
+
         double Xn = coordinates[coordinates.length-1][0];
         double Yn = coordinates[coordinates.length-1][1];
         double X1 = coordinates[0][0];
@@ -21,7 +19,6 @@ public class Current_Problem1232 {
         int c;      //constant
 
         if ( Yn-Y1 == 0) {// this means Y doesn't change and it'd have to be the same in all points for this to be true
-            System.out.println("Yn-Y1 == 0");
             for (int i = 1; i < coordinates.length; i++) {
                 if (coordinates[i][1] != Y1) {
                     return false;
@@ -30,7 +27,6 @@ public class Current_Problem1232 {
             return true;
         }
         else if (Xn-X1 == 0){  // that means X doesn't change, so it'd have to be the same in all points.
-            System.out.println("Xn-X1 == 0");
             for (int i=1; i < coordinates.length; i++){
                 if (coordinates[i][0]!=X1){
                     return false;
@@ -39,17 +35,13 @@ public class Current_Problem1232 {
             return true;
         }
         else{
-            System.out.println("Not a corner case");
-            System.out.println("("+Yn+"-"+Y1+")/("+Xn+"-"+X1+")");
             m =(Yn-Y1)/(Xn-X1);
-            System.out.println(m);
         }
 
         c = (int) (Y1 - m * X1);
 
         // y = m * x + c
         if (m==0){
-            System.out.println("slope is 0, so it's a constant");
             for (int i = 1; i < coordinates.length; i++) {
                 if (coordinates[i][1] != Y1) {
                     return false;
@@ -59,7 +51,6 @@ public class Current_Problem1232 {
 
         }
         else{
-
             for (int[] point: coordinates){
                 if (point[1]!=m*point[0]+c){
                     return false;
